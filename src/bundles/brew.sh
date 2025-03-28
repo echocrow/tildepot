134360de95a405380fa33a9ca1e9d5e2c72ec554
EchoCrow <5488190+echocrow@users.noreply.github.com>
2025-03-29 00:20:13 +0100

    fix broken brew commands after fresh install

--- src/bundles/brew.sh
+++ src/bundles/brew.sh
@@ -14,16 +14,16 @@ function PRE_INSTALL() {
 }
 
 function UPDATE() {
-  brew update
+  bundle::_brew update
   tilde::success "Updated Homebrew."
-  brew upgrade
+  bundle::_brew upgrade
   tilde::success "Updated Homebrew installations."
-  brew cleanup
+  bundle::_brew cleanup
   tilde::success "Cleaned up Homebrew installations."
 }
 
 function SNAPSHOT() {
-  brew bundle dump --force --no-vscode --file "$BREWFILE"
+  bundle::_brew bundle dump --force --no-vscode --file "$BREWFILE"
   tilde::success "Stored Homebrew dependencies to [$BREWFILE]."
 }
 
@@ -31,6 +31,39 @@ function APPLY_SKIP() {
   [ ! -f "$BREWFILE" ] && echo "No Brewfile present"
 }
 function APPLY() {
-  brew bundle install --force --cleanup --zap --file "$BREWFILE"
+  bundle::_brew bundle install --force --cleanup --zap --file "$BREWFILE"
   tilde::success "Restored Homebrew dependencies from [$BREWFILE]."
+}
+
+BUNDLE_BREW_CMD=""
+function bundle::_brew() {
+  if [ -z "$BUNDLE_BREW_CMD" ]; then
+    BUNDLE_BREW_CMD="$(bundle::brew_cmd)"
+  fi
+  "$BUNDLE_BREW_CMD" "$@"
+}
+
+function bundle::brew_cmd() {
+  tilde::cmd_exists brew && echo brew && return
+
+  # Determine brew bin path.
+  # @source https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+  if [[ "$(uname)" == "Darwin" ]]; then
+    if [[ "$(uname -m)" == "arm64" ]]; then
+      HOMEBREW_PREFIX="/opt/homebrew"
+    else
+      HOMEBREW_PREFIX="/usr/local"
+    fi
+  elif [[ "$(uname)" == "Linux" ]]; then
+    HOMEBREW_PREFIX="/home/linuxbrew/.linuxbrew"
+  else
+    tilde::error "Unknown platform." && exit 1
+  fi
+
+  local cmd="${HOMEBREW_PREFIX}/bin/brew"
+  if ! tilde::cmd_exists "$cmd"; then
+    tilde::error "Failed to determine Homebrew bin path." && exit 1
+  fi
+
+  echo "$cmd"
 }
