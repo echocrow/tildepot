694502caf576d795cae146251cba2c71bcf184e9
EchoCrow <5488190+echocrow@users.noreply.github.com>
2025-04-09 01:58:05 +0200

    feat(repo): support repo download w/o git

--- src/repo.sh
+++ src/repo.sh
@@ -57,6 +57,35 @@ function repo::create() {
   lib::ohai "Created tildepot repository at [$root]."
 }
 
+function repo::_optional_git_init() {
+  local root="$1"
+  local repo_origin="$2"
+  local branch="$3"
+
+  local remote="origin"
+  local tmp_user_mail="temp@temp.temp"
+
+  # This function is invoked inside of an `if` statement; `set -e` behavior is
+  # therefore unavailable.
+  true &&
+    git -C "$root" init --quiet -b "$branch" &&
+    git -C "$root" remote add "$remote" "$repo_origin" &&
+    git -C "$root" add . &&
+    git -C "$root" -c user.email="$tmp_user_mail" commit -m "Initial download" &&
+    git -C "$root" fetch &&
+    git -C "$root" branch -u "$remote"/"$branch" &&
+    git -C "$root" -c user.email="$tmp_user_mail" pull --rebase ||
+    return 1
+
+  # Reset temporary commit.
+  local last_committer
+  last_committer=$(git -C "$root" log -1 --pretty=format:"%ae")
+  if [[ $last_committer == "$tmp_user_mail" ]]; then
+    git -C "$root" reset HEAD~1 ||
+      return 1
+  fi
+}
+
 function repo::download() {
   local repo_origin="$1"
   repo_origin="$(repo::_get_origin_url "$repo_origin")"
@@ -64,7 +93,29 @@ function repo::download() {
   local root="$APP_REPO_ROOT"
   repo::_prepare_repo_root "$root"
 
-  git -C "$root" clone "$repo_origin" "$root"
+  # Attempt to download via git.
+  if ! tilde::cmd_exists git || ! git -C "$root" clone "$repo_origin" "$root"; then
+    # Fallback to download
+    lib::ohai "Could not clone repo via git; downloading instead..."
+    local remote="origin"
+    local branch="main"
+    local repo_origin_zip="${repo_origin%.git}/archive/refs/heads/$branch.zip"
+    local temp_file
+    temp_file=$(mktemp)
+    local temp_dir
+    temp_dir=$(mktemp -d)
+    lib::download "$repo_origin_zip" >"$temp_file" || lib::abort "Failed to download repository."
+    unzip -d "$temp_dir" "$temp_file" || lib::abort "Failed to download repository."
+    rm "$temp_file"
+    mv "$temp_dir"/*/* "$root"
+    rm -rf "$temp_dir"
+    if tilde::cmd_exists git; then
+      lib::ohai "Initializing downloaded git repo..."
+      if ! repo::_optional_git_init "$root" "$repo_origin" "$branch"; then
+        lib::warn "Failed to initialize git repo. Continuing without pulling."
+      fi
+    fi
+  fi
 
   lib::ohai "Downloaded tildepot repository to [$root]."
 }
