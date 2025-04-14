a27755b5c39afa1c6ecb91011516c8c619c50f95
EchoCrow <5488190+echocrow@users.noreply.github.com>
2025-04-15 01:03:00 +0200

    fix(app): fix formatting call in shared helper fns

--- src/shared.sh
+++ src/shared.sh
@@ -7,19 +7,19 @@ source "$(dirname "${BASH_SOURCE[0]}")/txt.sh"
 # Print a success message to stdout
 function tilde::success() {
   local messages=("$@")
-  printf "${txt_green}==>${txt_reset} %s\n" "$(lib::_ohai_fmt "${messages[@]}")"
+  printf "${txt_green}==>${txt_reset} %s\n" "$(lib::_fmt_msg "${messages[@]}")"
 }
 
 # Print a warning message to stdout
 function tilde::warning() {
   local messages=("$@")
-  printf "${txt_yellow}==>${txt_reset} %s\n" "$(lib::_ohai_fmt "${messages[@]}")" >&2
+  printf "${txt_yellow}==>${txt_reset} %s\n" "$(lib::_fmt_msg "${messages[@]}")" >&2
 }
 
 # Print a error message to stdout
 function tilde::error() {
   local messages=("$@")
-  printf "${txt_red}==>${txt_reset} %s\n" "$(lib::_ohai_fmt "${messages[@]}")" >&2
+  printf "${txt_red}==>${txt_reset} %s\n" "$(lib::_fmt_msg "${messages[@]}")" >&2
 }
 
 # Check if a command is installed
