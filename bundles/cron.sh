db3b94f63719aa2850ea754f86f7e255a766b16e
EchoCrow <5488190+echocrow@users.noreply.github.com>
2025-05-10 13:59:15 +0200

    feat(cron-bundle): support custom crontab names

--- bundles/cron.sh
+++ bundles/cron.sh
@@ -3,17 +3,40 @@
 # Tildepot bundle for crontab.
 # https://ss64.com/mac/crontab.html
 
-CRONTAB="$BUNDLE_DIR/crontab.txt"
-
 function SNAPSHOT() {
-  crontab -l >"$CRONTAB"
-  tilde::success "Stored crontab to [$CRONTAB]."
+  local crontab_path
+  crontab_path="$(bundle::_crontab_path)"
+
+  crontab -l >"$crontab_path"
+  tilde::success "Stored crontab to [$crontab_path]."
 }
 
 function APPLY_SKIP() {
-  [[ ! -f $CRONTAB ]] && echo "No snapshot present"
+  local crontab_path
+  crontab_path="$(bundle::_crontab_path)"
+
+  [[ ! -f $crontab_path ]] && echo "No snapshot present"
 }
 function APPLY() {
-  crontab "$CRONTAB"
-  tilde::success "Restored crontab from [$CRONTAB]."
+  local crontab_path
+  crontab_path="$(bundle::_crontab_path)"
+
+  crontab "$crontab_path"
+  tilde::success "Restored crontab from [$crontab_path]."
+}
+
+function bundle::crontab_name() {
+  # e.g. `hostname -s`
+  return
+}
+
+function bundle::_crontab_path() {
+  local name
+  name="$(bundle::crontab_name)"
+  if [[ -n $name ]]; then
+    echo "$BUNDLE_DIR/crontab_$(bundle::crontab_name).txt"
+  else
+    echo "$BUNDLE_DIR/crontab.txt"
+  fi
+
 }
