d659bdce676752572e259b34b072d716ea54bf26
EchoCrow <5488190+echocrow@users.noreply.github.com>
2025-05-10 13:35:12 +0200

    chore(release): allow bumping all bunches wit "every-bundle" scope

--- .releaserc.sh
+++ .releaserc.sh
@@ -14,6 +14,7 @@ function release_rc::main() {
     cfg="$(
       jq '.packages += [{
         "name": "'"${bundle}-bundle"'",
+        "scope": "'"${bundle}-bundle"'|every-bundle",
         "assets": ["'"bundles/${bundle}.sh"'"],
         "auxiliary": true
       }]' <<<"$cfg"
