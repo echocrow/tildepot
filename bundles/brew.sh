f121db56e0d3be208abc32eaea8bcfca49969f11
EchoCrow <5488190+echocrow@users.noreply.github.com>
2026-07-19 15:00:14 +0200

    fix(brew-bundle): only include brew installs in bundle dump

--- bundles/brew.sh
+++ bundles/brew.sh
@@ -23,7 +23,13 @@ function UPDATE() {
 }
 
 function SAVE() {
-	HOMEBREW_NO_AUTO_UPDATE=1 bundle::_brew bundle dump --force --no-describe --no-vscode --file "$BREWFILE"
+	HOMEBREW_NO_AUTO_UPDATE=1 bundle::_brew bundle dump \
+		--force \
+		--formula \
+		--cask \
+		--tap \
+		--no-describe \
+		--file "$BREWFILE"
 	tilde::success "Stored Homebrew dependencies to [$BREWFILE]."
 }
 
